0a6b0c3165539aba018954f0c3f907128dc8090e
Matheus Alexandre <matheuscurses@gmail.com>
2023-04-26 16:46:00 -0300

    Dapper com a pasta Models + arquivo Categoy.cs

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+using System;
+using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public Category()
+        {
+            Title ="";
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BaltaDataAccess.Models;
+using Dapper;
 using Microsoft.Data.SqlClient;
 
 namespace BaltaDataAccess
@@ -16,16 +18,12 @@ namespace BaltaDataAccess
             
             using(var command = new SqlCommand())
             {
-                command.Connection = connection;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                var reader = command.ExecuteReader();
-                while (reader.Read())
+               
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach(var category in categories)
                 {
-                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    Console.WriteLine($"{category.Id} - {category.Title}");
                 }
-
             }
 
          }
@@ -35,3 +33,10 @@ namespace BaltaDataAccess
     }
 }
 
+
+
+// Para começar a escrever a Query primeiro vc devevcriar uma pasta de Models
+// Para cada tabela no banco, vamos ter uma classe dela no C#
+// O Dapper vai pegar essa informação no banco e vai transformar em um objeto
+
+ // Vai no banco e executa a Query e traz a categorias em forma de lista
