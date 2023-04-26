f002e76794af9383e693e145fabd9a53f6c3ecbc
Matheus Alexandre <matheuscurses@gmail.com>
2023-04-26 12:48:11 -0300

    ADO.NET

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string connectionString = "Server=MAT-NOTE\\SQLEXPRESS;Database=balta;Integrated Security=SSPI";
+
+         using(var connection = new SqlConnection(connectionString))
+         {
+            Console.WriteLine("Conectado");
+            connection.Open();
+            
+            using(var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                }
+
+            }
+
+         }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
