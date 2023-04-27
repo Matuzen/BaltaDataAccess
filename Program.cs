31ea696510523d9dccf0b92c9e0110473ff00ee1
Matheus Alexandre <matheuscurses@gmail.com>
2023-04-27 11:50:21 -0300

    UPDATE e orgazizando o código em classes

--- Program.cs
+++ Program.cs
@@ -13,22 +13,36 @@ namespace BaltaDataAccess
 
             using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conectado");
-                connection.Open();
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }
+        }
 
-                var category = new Category();
-                // Gerar uma nova tabela
-                category.Id = Guid.NewGuid();
-                category.Title = "Amazon AWS";
-                category.Url = "amazon";
-                category.Description = "Categoria destinada a serviços do AWS";
-                category.Order = 8;
-                category.Summary = "AWS Cloud";
-                category.Featured = false;
-                
-                // SQL INJECTION (é um ataque conhecido e muito executado)
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
 
-                var insertSql = @"INSERT INTO [Category] 
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            // Gerar uma nova tabela
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            // SQL INJECTION (é um ataque conhecido e muito executado)
+
+            var insertSql = @"INSERT INTO [Category] 
                                 VALUES(
                                 @Id, 
                                 @Title, 
@@ -38,32 +52,29 @@ namespace BaltaDataAccess
                                 @Description, 
                                 @Featured)";
 
-                                // DAPPER
-                using (var command = new SqlCommand())
-                {
-                    // Retorna int com a quantidade de linhas afetadas
-                    var rows = connection.Execute(insertSql, new {
-                        category.Id,
-                        category.Title,
-                        category.Url,
-                        category.Summary,
-                        category.Order,
-                        category.Description,
-                        category.Featured
-                    });
-                    Console.WriteLine($"Linhas inseridas { rows}");
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"Linhas inseridas {rows}");
+        }
+    
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
+            // Executar a Query passando os parametros
+           var rows = connection.Execute(updateQuery, new{
+            id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+            title ="Frontend 2021"
+           });
 
-                    // Retorna uma lista 
-                    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                    foreach (var item in categories)
-                    {
-                        Console.WriteLine($"{item.Id} - {item.Title}");
-                    }
-                }
-
-            }
-
-            Console.WriteLine("Hello, World!");
+           Console.WriteLine($"Registros atualizados {rows}");
         }
     }
 }
