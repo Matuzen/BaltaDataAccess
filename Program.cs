a006700dc07ae8eb0c2ea249eb929fb3a8350f05
Matheus Alexandre <matheuscurses@gmail.com>
2023-04-27 09:47:39 -0300

    INSERT e mapeamento da tabela Category

--- Program.cs
+++ Program.cs
@@ -11,32 +11,33 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=MAT-NOTE\\SQLEXPRESS;Database=balta;Integrated Security=SSPI";
 
-         using(var connection = new SqlConnection(connectionString))
-         {
-            Console.WriteLine("Conectado");
-            connection.Open();
-            
-            using(var command = new SqlCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-               
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach(var category in categories)
-                {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
-                }
-            }
+                Console.WriteLine("Conectado");
+                connection.Open();
 
-         }
+                var category = new Category();
+                var insertSql = "INSERT INTO [Category] VALUES(id, title, url, summary, order, description, featured)";
+
+                using (var command = new SqlCommand())
+                {
+
+                    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                    foreach (var item in categories)
+                    {
+                        Console.WriteLine($"{item.Id} - {item.Title}");
+                    }
+                }
+
+            }
 
             Console.WriteLine("Hello, World!");
         }
     }
 }
 
-
-
 // Para começar a escrever a Query primeiro vc devevcriar uma pasta de Models
 // Para cada tabela no banco, vamos ter uma classe dela no C#
 // O Dapper vai pegar essa informação no banco e vai transformar em um objeto
 
- // Vai no banco e executa a Query e traz a categorias em forma de lista
+// Vai no banco e executa a Query e traz a categorias em forma de lista
