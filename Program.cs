eb36044ccb14afa543a1455ed4866cf208f3ccb4
Matheus Alexandre <matheuscurses@gmail.com>
2023-04-30 12:40:46 -0300

    Execute Many

--- Program.cs
+++ Program.cs
@@ -13,9 +13,10 @@ namespace BaltaDataAccess
 
             using (var connection = new SqlConnection(connectionString))
             {
-                UpdateCategory(connection);
+                //UpdateCategory(connection);
                 ListCategories(connection);
                 //CreateCategory(connection);
+                CreateManyCategory(connection);
             }
         }
 
@@ -64,18 +65,80 @@ namespace BaltaDataAccess
             });
             Console.WriteLine($"Linhas inseridas {rows}");
         }
-    
+
         static void UpdateCategory(SqlConnection connection)
         {
             var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
             // Executar a Query passando os parametros
-           var rows = connection.Execute(updateQuery, new{
-            id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
-            title ="Frontend 2021"
-           });
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Frontend 2021"
+            });
 
-           Console.WriteLine($"Registros atualizados {rows}");
+            Console.WriteLine($"Registros atualizados {rows}");
         }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            // Gerar uma nova tabela
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "categoria-nova";
+            category2.Description = "Categoria Nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria";
+            category2.Featured = true;
+
+
+            // SQL INJECTION (é um ataque conhecido e muito executado)
+
+            var insertSql = @"INSERT INTO [Category] 
+                                VALUES(
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order, 
+                                @Description, 
+                                @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]
+            {
+                new
+                {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+                },
+                new
+                {
+                category2.Id,
+                category2.Title,
+                category2.Url,
+                category2.Summary,
+                category2.Order,
+                category2.Description,
+                category2.Featured
+                }
+            });
+            Console.WriteLine($"Linhas inseridas {rows}");
+        }
+
     }
 }
 
